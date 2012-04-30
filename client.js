f0d7ee3e67f33c0e8da10740b8d5eea0fe20cb6c
Michael Bridgen <mikeb@squaremobius.net>
2012-04-30 12:38:13 +0100

    Cons a socket per backend. This will make hand-off easier (but not with pair sockets it won't)

--- client.js
+++ client.js
@@ -1,12 +1,22 @@
 var zmq = require('zmq');
 
-function Client(address) {
-  var socket = this._socket = zmq.socket('dealer');
+function Client(serviceAddr, bindAddr, connectAddr) {
+  this._serviceAddr = serviceAddr; // where to register
+  this._bindAddr = bindAddr; // where to wait for opens
+  this._connectAddr = connectAddr; // where to tell the bridge to send opens
+
+  var channel = this._channel = zmq.socket('pair');
+  var service = this._service = zmq.socket('dealer');
 
   var that = this;
-  socket.on('message', function(/* parts */) {
+  channel.on('message', function(/* parts */) {
     var command = arguments[0].toString();
     switch (command) {
+    case "HUP":
+      console.warn("Received HUP from bridge, re-registering with " +
+                   that._serviceAddr);
+      that.hello(); // and ...
+      break;
     case "OPEN":
       var id = arguments[1].toString();
       var detail = JSON.parse(arguments[2]);
@@ -26,31 +36,41 @@ function Client(address) {
     }
   });
 
-  socket.connect(address);
 }
 Client.prototype = new (require('events').EventEmitter)();
 
 Client.prototype.hello = function() {
-  this._socket.send("HELLO");
+  // errrrr
+  if (this._bound) {
+    this._service.send(["HELLO", this._connectAddr]);
+  }
+  else {
+    var that = this, service = this._service;
+    this._channel.bind(this._bindAddr, function() {
+      that._bound = true;
+      service.connect(that._serviceAddr);
+      service.send(["HELLO", that._connectAddr]);
+    });
+  }
 }
 Client.prototype.bye = function() {
-  this._socket.send("BYE");
+  this._channel.send("BYE");
 }
 Client.prototype.accept = function(id) {
-  this._socket.send(["ACCEPT", id]);
+  this._channel.send(["ACCEPT", id]);
 }
 Client.prototype.reject = function(id, reason) {
   reason = reason || "";
-  this._socket.send(["REJECT", id, reason]);
+  this._channel.send(["REJECT", id, reason]);
 }
 Client.prototype.send = function(data /*, connection ids */) {
   var parts = Array.prototype.slice.call(arguments);
   parts.unshift("SEND");
-  this._socket.send(parts);
+  this._channel.send(parts);
 }
 
 // === and finally
 
-module.exports.connect = function (addr) {
-  return new Client(addr);
+module.exports.connect = function (service, bind, connect) {
+  return new Client(service, bind, connect);
 };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var sockjs = require('sockjs'),
   zmq = require('zmq'),
   http = require('http'),
-  sys = require('sys');
+  util = require('util');
 
 var app = http.createServer();
 var socks = sockjs.createServer({prefix: '/socks'});
@@ -11,53 +11,71 @@ var service = zmq.socket('router');
 service.on('message', function(id) {
   var command = arguments[1].toString();
   var args = Array.prototype.slice.call(arguments, 2);
-  console.log({from: id, args: args});
   switch (command) {
   case "HELLO":
-    console.log({hello: id});
-    addRouter(id);
+    addr = args[0].toString();
+    console.log({hello: addr});
+    addRouter(addr);
     break;
-  case "BYE":
-    console.log({bye: id});
-    removeRouter(id);
-    break;
-  case "ACCEPT":
-    console.log({accept: args[0]});
-    acceptConnection(args[0]);
-    break;
-  case "REJECT":
-    console.log({reject: args[0]});
-    rejectConnection(args[0]);
-    break;
-  case "SEND":
-    var data = args[0];
-    console.log({send: data});
-    for (var i = 1, len = args.length; i < len; i++) {
-      sendOnConnection(args[i], data);
-    }
-    break;
-  case "CONT":
-    // TODO
-    break;
-  default:
-    console.warn({unknown: command, args: args});
   }
 });
+
+function wireChannel(id, channel) {
+  channel.on('message', function() {
+    var command = arguments[0].toString();
+    var args = Array.prototype.slice.call(arguments, 1);
+    console.log({service: id, args: args});
+    switch (command) {
+    case "BYE":
+      console.log({bye: id});
+      removeRouter(id);
+      break;
+    case "ACCEPT":
+      console.log({accept: args[0]});
+      acceptConnection(args[0]);
+      break;
+    case "REJECT":
+      console.log({reject: args[0]});
+      rejectConnection(args[0]);
+      break;
+    case "SEND":
+      var data = args[0];
+      console.log({send: data});
+      for (var i = 1, len = args.length; i < len; i++) {
+        sendOnConnection(args[i], data);
+      }
+      break;
+    case "CONT":
+      // TODO
+      break;
+    default:
+      console.warn({unknown: command, args: args});
+    }
+  });
+}
 
 // takeover handling
 process.on('SIGUSR1', function() {
   console.log("Received SIGUSR1, freeing registration port");
   // free the port
   service.close();
+  // tell all the routers to re-register
+  for (var i = 0, len = routers.length; i < len; i++) {
+    routers[i].socket.send("HUP");
+  }
 });
 process.on('SIGUSR2', function() {
   console.log("Received SIGUSR2, freeing HTTP port");
   app.close();
+  // TODO and close channel and exit, when all routers have gone away.
 });
 
 routers = [];
-function addRouter(id) {
-  routers.push(id);
+function addRouter(addr) {
+  var channel = zmq.socket('pair');
+  wireChannel(addr, channel);
+  channel.connect(addr);
+  routers.push({id: addr, socket: channel});
 }
 function nextRouter() {
   var router = routers.shift();
@@ -65,21 +83,21 @@ function nextRouter() {
   return router;
 }
 function removeRouter(id) {
-  var idStr = id.toString();
   for (var i = 0, len = routers.length; i < len; i++) {
-    if (routers[i].toString() == idStr) {
-      routers.splice(i, 1);
+    if (routers[i].id == id) {
+      var closed = routers.splice(i, 1);
+      closed[0].socket.close();
       return;
     }
   }
-  console.warn("BYE from unknown router: " + sys.inspect(id));
+  console.warn("BYE from unknown router: " + util.inspect(id));
 }
 
 pending = {};
 open = {};
 
-function parkConnection(id, connection) {
-  pending[id] = connection;
+function parkConnection(id, router, connection) {
+  pending[id] = {router: router, connection: connection};
 }
 function acceptConnection(id) {
   var conn = pending[id];
@@ -94,7 +112,7 @@ function acceptConnection(id) {
 function rejectConnection(id, connection) {
   var conn = pending[id];
   if (conn) {
-    conn.close(500, "Connection rejected"); // proper code
+    conn.connection.close(500, "Connection rejected"); // proper code
     delete pending[id];
   }
   else {
@@ -105,7 +123,7 @@ function rejectConnection(id, connection) {
 function sendOnConnection(id, data) {
   var conn = open[id];
   if (conn) {
-    conn.write(data);
+    conn.connection.write(data);
   }
   else {
     console.warn("Send on unknown (or unaccepted) connection: " + id);
@@ -117,15 +135,14 @@ socks.on('connection', function(conn) {
   console.log({connection: id});
   var router = nextRouter();
   if (router) {
-    parkConnection(id, conn);
+    parkConnection(id, router, conn);
     var detail = detailOf(conn);
-    pending[id] = conn;
-    service.send([router, "OPEN", id, JSON.stringify(detail)]);
+    router.socket.send(["OPEN", id, JSON.stringify(detail)]);
   }
   else {
     // TODO Something more constructive
     console.warn("No router to send to! Closing connection");
-    conn.close(500, "No service"); // TODO proper error code.
+    conn.connection.close(500, "No service"); // TODO proper error code.
   }
 });
 
@@ -135,6 +152,11 @@ service.bind('tcp://*:5000', function() {
 });
 
 console.log("Running as pid " + process.pid);
+
+module.exports = {pending: pending,
+                  open: open,
+                  service: service,
+                  routers: routers};
 
 // ============= gubbins ==============
 
